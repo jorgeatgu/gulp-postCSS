d48aeb86fe30c3c7ddb5baf9382060fca4143c28
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2015-11-25 10:52:01 +0100

    finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
+var vars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var rucksack = require('gulp-rucksack');
+var pxtorem = require('postcss-pxtorem');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('css', function() {
 	var processors = [
-		//Aqui irán los plugins que vayamos instalando
+		vars,
+		nested,
+		rucksack,
+		pxtorem({
+			root_value: 16,
+			unit_precision: 2,
+			prop_white_list: ['font-size', 'line-height', 'padding'],
+			replace: true,
+			media_query: false
+		}),
+		autoprefixer
 	];
 	//Aquí la ruta de donde coge nuestros css
 	return gulp.src('./src/css/styles.css')
+		.pipe(rucksack())
+		.pipe(sourcemaps.init())
 		.pipe(postcss(processors))
-		//Aqui la ruta de destino
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./dist/css'));
+});
+
+
+gulp.task('default', function() {
+	gulp.watch('./src/css/*.css', ['css']);
 });
