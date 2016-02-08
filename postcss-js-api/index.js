412df0bfdb9c7980af61e9ce76d506d37b7c8fe3
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2016-02-08 21:27:44 +0100

    retocando la configuracion para postCSS JS API

--- postcss-js-api/index.js
+++ postcss-js-api/index.js
@@ -1,22 +1,40 @@
 var fs = require("fs")
+var autoprefixer = require("autoprefixer")
 var postcss = require("postcss")
+var atImport = require("postcss-import")
+var vars = require("postcss-simple-vars")
+var nested = require("postcss-nested")
+var pxToRem = require("postcss-pxtorem")
+var sorting = require("postcss-sorting")
+var devtools = require('postcss-devtools')
 var css = fs.readFileSync("src/css/styles.css", "utf8")
 
-postcss([require('autoprefixer'), require('postcss-import'), require('postcss-simple-vars'), require('postcss-nested'), require('postcss-pxtorem')({
-	root_value: 16,
-	unit_precision: 2,
-	prop_white_list: ['font-size', 'line-height', 'padding'],
-	replace: true,
-	media_query: false
-}), require('postcss-sorting'), require('postcss-devtools')])
-.process(css, {
+postcss()
+	.use(devtools())
+	.use(atImport())
+	.use(vars())
+	.use(nested())
+	//pasamos las opciones al plugin
+	.use(pxToRem({
+		root_value: 16,
+		unit_precision: 2,
+		prop_white_list: ['font-size', 'line-height', 'padding'],
+		replace: true,
+		media_query: false
+	}))
+	.use(sorting())
+	.process(css, {
+		//La ruta donde se alojan nuestros CSS
 		from: "./src/css/styles.css",
+		//La ruta donde vamos a dejar el CSS
 		to: "./css/styles.css",
+		//Vamos a desactivar los source maps inline
 		map: {
 			inline: false
 		},
 	})
 	.then(function(result) {
 		fs.writeFileSync('./css/styles.css', result.css);
+		//Si desactivamos los source maps inline vamos a escribir la ruta donde queremos que cree el .map
 		if (result.map) fs.writeFileSync('./css/styles.css.map', result.map);
 	});
