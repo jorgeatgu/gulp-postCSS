f3a109e15d434d031fb73d97274094c4d999c0ca
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2016-05-05 19:24:36 +0200

    gulpfile & package.json :arrow_up:

---
+++ gzip-postcss-sorting/gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+postcss = require('gulp-postcss');
+autoprefixer = require('gulp-autoprefixer');
+sourcemaps = require('gulp-sourcemaps');
+atImport = require('postcss-import');
+postCSS_InlineComment = require('postcss-inline-comment');
+cssnext = require('postcss-cssnext');
+sorting = require('postcss-sorting');
+nested = require('postcss-nested');
+nano = require('gulp-cssnano');
+
+
+var imgSrc = './src/img/*';
+var imgDist = './img';
+var jsSrc = './src/js/*.js';
+var jsDist = './js';
+
+
+gulp.task('css', function() {
+    var processors = [
+        nested,
+        atImport,
+        cssnext,
+        autoprefixer,
+        sorting({
+            "sort-order": "csscomb"
+        })
+    ];
+    return gulp.src('./src/css/styles.css')
+
+    .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .on("error", errorAlertPost)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'))
+        .pipe(notify({
+            message: 'postCSS complete'
+        }));
+});
+
+gulp.task('minify', function() {
+    return gulp.src('./css/styles.css')
+        .pipe(nano())
+        .pipe(gulp.dest('./css'))
+        .pipe(notify({
+            message: 'CSSnano task complete'
+        }));
+});
+
+
+gulp.task('default', function() {
+    gulp.watch('./src/css/*.css', ['css']);
+    gulp.watch('./css/*.css', ['minify']);
+});
