4eb1b7b6e5f2564a2f1c1974cc25d9dfe84d2e7c
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2016-05-05 20:10:24 +0200

    cambios en gulp

--- gzip-postcss-sorting/gulpfile.js
+++ gzip-postcss-sorting/gulpfile.js
@@ -30,21 +30,14 @@ gulp.task('css', function() {
 
     .pipe(sourcemaps.init())
         .pipe(postcss(processors))
-        .on("error", errorAlertPost)
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'))
-        .pipe(notify({
-            message: 'postCSS complete'
-        }));
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('minify', function() {
     return gulp.src('./css/styles.css')
         .pipe(nano())
-        .pipe(gulp.dest('./css'))
-        .pipe(notify({
-            message: 'CSSnano task complete'
-        }));
+        .pipe(gulp.dest('./css'));
 });
 
 
